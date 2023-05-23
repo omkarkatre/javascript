877e6f56bed08aecc309e3d98b0e283ec95a8093
omkarkatre <omkarkatre003@gmail.com>
2023-05-23 07:31:26 +0530

    variable
    variable

---
+++ 01_variable_assign.js
@@ -0,0 +1,18 @@
+var myFullName="Omkar katre"
+console.log(myFullName);
+var myLove="I love only JS"
+console.log(myLove);
+var  firstName="Omkar"
+console.log(firstName);
+var  LastName="Katre"
+console.log(LastName);
+var  age="22"
+console.log(22);
+var  address="At post-kole,Tal-karad,Dist-Satara"
+console.log(address);
+var pincode="415110"
+console.log(415110);
+var Gender="male"
+console.log(Gender);
+var ismarried="no"
+console.log(ismarried);
