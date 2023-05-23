e014c4fce565618f7ddeff1ded56735e6fe2d4cc
omkarkatre <omkarkatre003@gmail.com>
2023-05-23 07:39:36 +0530

    Basic Assign
    Basic Assign

---
+++ 02_basicAssign.js
@@ -0,0 +1,39 @@
+var Pin = 415110;
+var typeofpin= typeof Pin;
+console.log(" pin :",Pin);
+console.log("Type:",typeof Pin);
+var number = 89;
+var Typeofno= typeof number;
+console.log("Number:",number);
+console.log("Type:",typeof number);
+var mobno = 123456789;
+var Mobno= typeof mobno;
+console.log("Mob no:",mobno);
+console.log("Type:",typeof mobno);
+console.log("------------- string ---------------");
+
+var firstName = "Omkar";
+console.log("firstName:",firstName);
+console.log("Type:",typeof firstName);
+var surname="Katre";
+console.log("surname:",surname);
+console.log("Type:",typeof surname );
+var state="Maharastra";
+console.log("state:",state);
+console.log("Type",typeof state);
+console.log("--------Boolen----------");
+var isMarried = true;
+console.log("isMarried:",isMarried);
+console.log("Type",typeof isMarried);
+var knowAngular = true; 
+console.log("knowAngular;",knowAngular );
+console.log("type:",typeof knowAngular );
+var isBachelor=false;
+console.log("isBachelor",typeof isBachelor);
+console.log("-----------Diff between undefined and null--------");
+console.log("undefined means variable is declared but its value is not assigned and");
+console.log("null means  variable is declared but its value is assigned as null value");
+console.log("so we get typeof undefined as undefined and typeof null is object");
+
+
+
