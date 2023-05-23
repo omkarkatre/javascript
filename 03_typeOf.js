93e5028821ce2e2dcbf13a699cb585afdf102f1f
omkarkatre <omkarkatre003@gmail.com>
2023-05-23 07:46:28 +0530

    types
    Types of

---
+++ 03_typeOf.js
@@ -0,0 +1,20 @@
+var bankName=
+console.log("bankName:",bankName);
+console.log("type:",typeof bankName );
+var bankName= "SBI Bank"
+console.log("bankName:",bankName);
+console.log("type:",typeof bankName );
+console.log("--------------");
+var marks="90%"
+console.log(typeof marks,marks);
+console.log("-------------");
+var isWorking=
+console.log("value before intaliztion:",isWorking);
+var isWorking=true
+console.log("value After intaliztion:",isWorking);
+console.log("--------------------");
+var totalCount="Hundared and Seven"
+console.log("valve and datatype of totalCount:",typeof totalCount,totalCount );
+var totalCount=107
+console.log("valve and datatype of totalCount:",typeof totalCount,totalCount );
+
