f245f135cb21a6cab6ac0db8aa763ea4c2884709
omkarkatre <omkarkatre003@gmail.com>
2023-05-30 12:48:41 +0530

    operator A
    operator A

---
+++ operatorA.js
@@ -0,0 +1,40 @@
+var string1="JavaScript";
+var string2="Google Chrome";
+var string3="Developer Smart";
+var string4="I am Angular Developer"
+console.log(`Length of word & its square1`);
+function squareOfWorldLength(arg)
+{
+    var result=arg.length;
+    console.log("total words length:",result);
+    var result1= result*result;
+    console.log("Square of this :",result1);
+
+}
+squareOfWorldLength("JavaScript");
+squareOfWorldLength("Google Chrome");
+squareOfWorldLength("Developer Smart");
+console.log("--------------");
+console.log("Divide and multiplication");
+function stringhandsOn() {
+    console.log("2.1");
+var stringlength=string4.length;
+console.log(`Length of string is => ${stringlength}`);
+var resultSplit=string4.split("");
+//console.log(`Total number of word ->${resultSplit}`);
+var num1="22";
+var num2="4";
+var result3=num1/num2;
+console.log(`Division--> ${result3}`);
+console.log(`total no og word ->${num2}`);
+console.log(`-----------`);
+var result4=num1*num2;
+console.log(`multiplication--> ${result4}`);
+console.log(`total no og word ->${num2}`);
+console.log(`-----------`);
+
+
+
+}
+stringhandsOn();
+
